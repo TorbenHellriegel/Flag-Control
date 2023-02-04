deb420bde4d6cdbc520220a805bbf15531f81cc0
Jan Reimer <reimer-jan@gmx.de>
2023-02-04 14:42:45 +0100

    Quit Button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -31,4 +31,10 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
 }
